03b8190fb321fa6d4090db113f9a30df3ad90087
Nick <nikngeno@gmail.com>
2024-09-21 15:17:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Fermat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckFermat(3,4,5,3);
+        }
+        static void CheckFermat(int a, int b, int c, int n)
+        {
+            if (n > 2)
+            {
+                double sum = Math.Pow(a, n) + Math.Pow(b, n);
+                double equal = Math.Pow(c, n);
+
+                if (sum == equal)
+                {
+                    Console.WriteLine("Holy smokes, Fermat was wrong!");
+                }
+                else
+                {
+                    Console.WriteLine("No, that doesn't work.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No, that doesn't work.");
+            }
+        }
+    }
+}
